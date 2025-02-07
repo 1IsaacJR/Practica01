7cd753033aa8f512f8a75f92769a22aa55322b6a
Isaac <irj@ciencias.unam.mx>
2025-02-07 11:54:02 -0600

    Ejercicio6 mejora

--- Ejercicio6.hs
+++ Ejercicio6.hs
@@ -1,22 +1,27 @@
 --Ejercicio6
 --Restriccion de Dominio meidiante Patrones
 {-
---Falta completar
-
 primoPatrones:: Integer -> Bool 
-primoPatrones n `mod` 2 ==    = False
-primoPatrones n `mod` 2 ==    = True
-
+primoPatrones `mod` 2 =  False
+primoPatrones > 1 =  True
 -}
 
 --Restriccion de Dominio mediante Guardas
 primoGuardas:: Integer -> Bool
 primoGuardas n
-      | n `mod` 2  == 0    = False
-      | n `mod` 2  /= 0    = True
+      | n <= 1 	      		= False
+      | n == 2 			= True	
+      | n `mod` 2  == 0		= False
+      | n `mod` 2  /= 0    	= True
 
 --Falta metodo para comparar las dos funciones
+{-
+primoComparacion:: (primoPatrones a, primoGuardas a) -> Bool
+primoComparacion n = if primoGuardas(n) ==  primoPatrones(n) then True else False
+-}
 
 main = do
-     let n = 10
+     let n = -1
+     --let m = 10
      print(primoGuardas n)
+     --print(primoPatrones m)
