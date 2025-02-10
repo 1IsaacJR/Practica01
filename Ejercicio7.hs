b2694b3b5198d5621a8dfbd3f84aebab1212e0c2
Isaac <irj@ciencias.unam.mx>
2025-02-09 22:05:25 -0600

    ejercicio 1 y 2

--- Ejercicio7.hs
+++ Ejercicio7.hs
@@ -4,6 +4,17 @@ f:: Integer -> Bool
 esFormula, reccorrido:: String -> Bool -> list[] ->
 recorrido n k = if n = 1 then True
 	      else recoorido(n)
+	      
 esFormula n = if n == E then True
 	      else false
-	
+
+{-
+E(1) = E
+Pertenece al alfabeto
+E(1) = _
+Pertenece al alfabeto
+E(1) = EEE
+Cualquier composicion pertenece al afabeto
+Combinacion de todas las composiciones de las cadenas posibles para culquier alfabeto
+
+-}
